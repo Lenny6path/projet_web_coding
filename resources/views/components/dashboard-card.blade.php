9b83571a3908ba16ebae769e8317a65947a23375
Lenny6path <lenny.mallot@edu.esiee-it.fr>
2025-04-17 16:26:17 +0200

    story 7

--- resources/views/components/dashboard-card.blade.php
+++ resources/views/components/dashboard-card.blade.php
@@ -1,6 +1,6 @@
 {{-- resources/views/components/dashboard-card.blade.php --}}
 @props(['title', 'count', 'route'])
-<a href="{{ $route }}" class="bg-white border border-gray-200 rounded-2xl shadow-sm hover:shadow-lg transition-all p-5 flex flex-col justify-between text-center min-w-[120px]">
+<a href="{{ $route }}" class="bg-white rounded-2xl hover:shadow-lg transition-all p-2 flex flex-col justify-between text-center ">
     <div class="text-gray-600 text-base font-medium break-words">
         {{ $title }}
     </div>
